34946685b78441fbe6c3e60bc6bce81c97f853c3
otumian-empire <popecan1000@gmail.com>
2023-06-11 19:25:42 +0000

    flutter push

---
+++ lib/main.dart
@@ -0,0 +1,38 @@
+import 'dart:html';
+
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'OE Flutter Hands',
+      theme: ThemeData(
+        primarySwatch: Colors.lightBlue,
+      ),
+      home: const WelcomePage(),
+    );
+  }
+}
+
+class WelcomePage extends StatelessWidget {
+  const WelcomePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Welcome"),
+        centerTitle: true,
+        backgroundColor: Colors.lightBlueAccent,
+      ),
+    );
+  }
+}
